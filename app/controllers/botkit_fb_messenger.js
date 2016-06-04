6eebe7f6d15ff5dc0c2ff0fd49b0be9dc94c0844
Peter Marko <pemoke@gmail.com>
2016-06-04 23:19:11 +1000

    Structured message with Edamam API call

--- app/controllers/botkit_fb_messenger.js
+++ app/controllers/botkit_fb_messenger.js
@@ -2,6 +2,7 @@
 
 /// modules
 const Botkit = require('botkit');
+const rest = require('restler');
 
 // messenger API parameters
 const FB_PAGE_ID = process.env.FB_PAGE_ID;
@@ -25,12 +26,53 @@ const controller = Botkit.facebookbot({
 
 const bot = controller.spawn({});
 
-
 // user said hello
 controller.hears(['hi'], 'message_received', function (bot, message) {
-  bot.reply(message, 'Hey there.');
+  bot.reply(message, 'Hey there, what do you feel like eating?');
 });
 
+// user says anything else
+controller.hears('(.*)', 'message_received', function (bot, message) {
+  let url = 'https://api.edamam.com/diet?app_id=d5933c85&app_key=58a44858bd37d448279ba6bfbced33a9&q=' + message.text;
+  rest.get(url).on('complete', function(data) {
+    let dataElements = [];
+    let resultsCount = data.count > 10 ? 10 : data.count;
+    let structuredMessage = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": dataElements
+        }
+      }
+    };
+
+    if (resultsCount) {
+      for (let i = 0; i < resultsCount; i++) {
+        dataElements.push({
+          "title": data.hits[i].recipe.label,
+          "subtitle": data.hits[i].recipe.source,
+          "image_url": data.hits[i].recipe.image,
+          "buttons": [{
+            "type": "web_url",
+            "url": data.hits[i].recipe.url,
+            "title": "Open website"
+          },
+          // {
+          //   "type": "postback",
+          //   "title": "Postback",
+          //   "payload": `Payload for ${data.hits[i].recipe.label} element in a generic bubble`,
+          // }
+          ],
+        });
+      }
+      bot.reply(message, structuredMessage);
+    } else {
+      bot.reply(message, `Sorry, didn't find anything for: ${message.text}`);
+    }
+
+  });
+});
 
 // logic
 const processMessage = (fbMsg) => {
